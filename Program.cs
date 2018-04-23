6eecf0f29c0a776c54e1a1a58cf6b283190a91de
Logan Drescher <emaildrescher@gmail.com>
2018-04-22 21:30:29 -0400

    Init commit, nothing interesting yet

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AnimaCCv1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Summoning.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimaCCv1
+{
+    class Summoning
+    {
+    }
+}
